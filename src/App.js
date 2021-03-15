12341729487d5e9b33b85e52dbfa480d3924ffc8
Derek David <derekmiracledavid@hotmail.com>
2021-03-15 20:27:36 +0100

    App.js modified to support redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./containers/Home";
-
+import { Provider } from "react-redux";
+import store from "./store";
+import ProductDetail from "./containers/ProductDetail";
+import AddToCart from "./containers/AddToCart";
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Route exact path="/" component={Home} />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/:id" component={ProductDetail} />
+        <Route exact path="/cart/:id" component={AddToCart} />
+      </BrowserRouter>
+    </Provider>
   );
 };
 
