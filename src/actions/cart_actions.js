500fb83de4e9b3b017e3465e12377af51dd41f23
Derek David <derekmiracledavid@hotmail.com>
2021-03-16 15:15:55 +0100

    minor change to addToCart action

--- src/actions/cart_actions.js
+++ src/actions/cart_actions.js
@@ -3,7 +3,7 @@ import { CART_ADD } from "./cart";
 export const addToCart = (id, qty) => (dispatch) => {
   Axios.get(`http://localhost:5000/api/products/${id}`)
     .then((res) => {
-      dispatch({ type: CART_ADD, payload: res.data });
+      dispatch({ type: CART_ADD, payload: { data: res.data, qty } });
     })
     .catch((err) => {
       alert(err);
