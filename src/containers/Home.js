9a6438280bf95c868a42bd570a36d3e0c3b63f37
Derek David <derekmiracledavid@hotmail.com>
2021-03-16 15:24:59 +0100

    product_ id passed as a prop to ProductHome component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -49,7 +49,7 @@ const Home = () => {
                   src={product.imgUrl}
                   title={product.name}
                   price={product.price}
-                  url={product._id}
+                  url={`products/${product._id}`}
                   rating={product.rating}
                 />
               ))}
