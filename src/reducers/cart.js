c8a7e36978fba2cf4545a96d3d108afa89ccab81
Derek David <derekmiracledavid@hotmail.com>
2021-03-16 15:29:53 +0100

    addToCart reducers created

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,12 +1,19 @@
 import { CART_ADD } from "../actions/cart";
 
-export const addToCart = (state = { cart: [] }, action) => {
+export const addToCart = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD:
-      const item = action.payload;
-      return {
-        cart: state.cart.map((x) => item),
-      };
+      const item = { ...action.payload.data, quantity: action.payload.qty };
+      const itemExists = state.cartItems.find((x) => x._id === item._id);
+      if (itemExists) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x._id === itemExists._id ? item : x
+          ),
+        };
+      }
+      return { ...state, cartItems: [...state.cartItems, item] };
     default:
       return state;
   }
