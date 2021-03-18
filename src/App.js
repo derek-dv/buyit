67876b4d7a335def28770def973b20d6719b72da
Derek David <derekmiracledavid@hotmail.com>
2021-03-19 00:52:02 +0100

    login, add to cart and register routes added. Provider and redux store moved to index.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./containers/Home";
-import { Provider } from "react-redux";
-import store from "./store";
+import { useSelector } from "react-redux";
 import ProductDetail from "./containers/ProductDetail";
 import AddToCart from "./containers/AddToCart";
+import Login from "./containers/Login";
+import Register from "./containers/Register";
+import Alert from "./components/Alert";
 const App = () => {
+  const alerts = useSelector((state) => state.alerts);
+
+  // useEffect(() => {
+
+  // })
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <div className="App">
+        {alerts ? alerts.map((alert) => <Alert alert={alert} />) : null}
         <Route exact path="/" component={Home} />
         <Route exact path="/products/:id" component={ProductDetail} />
-        <Route exact path="/cart/:id" component={AddToCart} />
-      </BrowserRouter>
-    </Provider>
+        <Route exact path="/cart" component={AddToCart} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </div>
+    </BrowserRouter>
   );
 };
 
