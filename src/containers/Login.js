3dc11551e15018a9a1c977310a6d4892158520df
Derek David <derekmiracledavid@hotmail.com>
2021-03-19 01:07:15 +0100

    Login and register pages created here

---
+++ src/containers/Login.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { login } from "../actions/auth_actions";
+import "../styles/Auth.css";
+
+const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const redirect = props.location.search
+    ? props.location.search.split("=")[1]
+    : "";
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login({ email, password }));
+    <Redirect to="/" />;
+  };
+  return (
+    <div className="auths">
+      <div class="auth">
+        <div className="auth__box">
+          <h2 className="auth__heading">Login</h2>
+          <form onSubmit={submitHandler}>
+            <div className="auth__group">
+              <label htmlFor="email">Email</label>
+              <input
+                className="auth__input"
+                required
+                placeHolder="Enter Email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="auth__group">
+              <label htmlFor="password">Password</label>
+              <input
+                className="auth__input"
+                type="password"
+                required
+                placeHolder="Enter Password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="auth__group">
+              <button type="submit" className="auth__button">
+                Login
+              </button>
+            </div>
+            <div className="auth__group">
+              <p className="auth__text">
+                Don't have an account? <Link to="/register">Register</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/containers/Register.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { addAlert } from "../actions/alert_actions";
+import { v4 } from "uuid";
+import "../styles/Auth.css";
+import { register } from "../actions/auth_actions";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password1 !== password2) {
+      dispatch(addAlert(v4(), "Passwords do not match", "error"));
+    } else {
+      dispatch(register({ name, email, password: password1 }));
+    }
+  };
+
+  return (
+    <div className="auth">
+      <div className="auth__box">
+        <h2 className="auth__heading">Create Account</h2>
+        <form onSubmit={onSubmit}>
+          <div className="auth__group">
+            <label htmlFor="email">Name</label>
+            <input
+              className="auth__input"
+              required
+              placeHolder="Enter Name"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="auth__group">
+            <label htmlFor="email">Email</label>
+            <input
+              className="auth__input"
+              type="email"
+              required
+              placeHolder="Enter Email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="auth__group">
+            <label htmlFor="password">Password</label>
+            <input
+              className="auth__input"
+              type="password"
+              required
+              placeHolder="Enter Password"
+              id="password"
+              value={password1}
+              onChange={(e) => setPassword1(e.target.value)}
+            />
+          </div>
+          <div className="auth__group">
+            <label htmlFor="password">Confirm Password</label>
+            <input
+              className="auth__input"
+              type="password"
+              required
+              placeHolder="Confirm Password"
+              id="password2"
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)}
+            />
+          </div>
+          <div className="auth__group">
+            <button type="submit" className="auth__button">
+              Register
+            </button>
+          </div>
+          <div className="auth__group">
+            <p className="auth__text">
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
